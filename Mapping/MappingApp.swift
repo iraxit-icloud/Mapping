c00bd6df876729ab468ad99c3146e2964edf0f74
iraxit-cloud <indraneel.raxit@icloud.com>
2025-09-19 12:26:11 -0400

    Initial Commit

---
+++ Mapping/MappingApp.swift
@@ -0,0 +1,17 @@
+//
+//  MappingApp.swift
+//  Mapping
+//
+//  Created by Indraneel Rakshit on 9/19/25.
+//
+
+import SwiftUI
+
+@main
+struct MappingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MappingTests/MappingTests.swift
@@ -0,0 +1,17 @@
+//
+//  MappingTests.swift
+//  MappingTests
+//
+//  Created by Indraneel Rakshit on 9/19/25.
+//
+
+import Testing
+@testable import Mapping
+
+struct MappingTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
